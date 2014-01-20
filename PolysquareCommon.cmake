a996b46643111831624d86c83b2b93b4ea001a90
Sam Spilsbury <smspillaz@gmail.com>
2014-01-20 17:49:01 -0200

    Initial bootstrap for common-universal-cmake.
    Bootstrap the components that we'll be using in our own macros,
    including:
    
     - cppcheck-target-cmake
     - gcov-cmake
     - gmock-cmake
     - veracpp-cmake

---
+++ PolysquareCommon.cmake
@@ -0,0 +1,11 @@
+# /PolysquareCommon.cmake
+# Provides some functionality that is common to all polysquare projects,
+# such as bootstrapping static analysis tools, adding code coverage
+# targets and convience functions to add tests, matchers, mocks etc
+# without having to write too much boilerplate.
+#
+# This file should not be included directly as it assumes that
+# CMAKE_CURRENT_SOURCE_DIRECTORY is the directory that this
+# file currently resides in.
+#
+# See LICENCE.md for Copyright information
