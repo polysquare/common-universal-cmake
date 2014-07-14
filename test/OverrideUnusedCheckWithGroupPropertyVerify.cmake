8b464994571f5ce23226979a7adfd4d2de6a0718
Sam Spilsbury <smspillaz@gmail.com>
2014-07-14 13:52:55 +0800

    Build using Makefiles and Ninja

--- test/OverrideUnusedCheckWithGroupPropertyVerify.cmake
+++ test/OverrideUnusedCheckWithGroupPropertyVerify.cmake
@@ -8,9 +8,9 @@ include (${POLYSQUARE_COMMON_UNIVERSAL_CMAKE_TESTS_DIRECTORY}/CMakeUnit.cmake)
 
 set (BUILD_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/BUILD.output)
 
-set (UNUSED_CHECK_LIBRARIES_COMMENT
-     "Running unused function check\\: unused_check_libraries")
+set (UNUSED_CHECK_LIBRARIES_STAMP
+     "^.*unused_check_libraries.stamp.*$")
 assert_file_has_line_matching (${BUILD_OUTPUT}
-                               ${UNUSED_CHECK_LIBRARIES_COMMENT})
+                               ${UNUSED_CHECK_LIBRARIES_STAMP})
 assert_file_has_line_matching (${BUILD_OUTPUT}
                                "^.*cppcheck.*unusedFunction.*Source\\.cpp.*$")
